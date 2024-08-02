550c3b5771d7ca6d6460e6dcd35a95f9b9765e0b
henriqbr14 <henriqschelotag@gmail.com>
2024-08-02 20:18:36 -0300

    Primeira Etapa

--- terceiroprojeto/Form1.Designer.cs
+++ terceiroprojeto/Form1.Designer.cs
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            TbValor1 = new TextBox();
+            TbValor2 = new TextBox();
+            lblvalor2 = new Label();
+            lblvalor1 = new Label();
+            LblTotal = new Label();
+            LblResultado = new Label();
+            btnsoma = new Button();
+            SuspendLayout();
+            // 
+            // TbValor1
+            // 
+            TbValor1.Location = new Point(142, 60);
+            TbValor1.Name = "TbValor1";
+            TbValor1.Size = new Size(100, 23);
+            TbValor1.TabIndex = 0;
+            // 
+            // TbValor2
+            // 
+            TbValor2.Location = new Point(142, 118);
+            TbValor2.Name = "TbValor2";
+            TbValor2.Size = new Size(100, 23);
+            TbValor2.TabIndex = 1;
+            // 
+            // lblvalor2
+            // 
+            lblvalor2.AutoSize = true;
+            lblvalor2.Location = new Point(94, 121);
+            lblvalor2.Name = "lblvalor2";
+            lblvalor2.Size = new Size(42, 15);
+            lblvalor2.TabIndex = 2;
+            lblvalor2.Text = "Valor 2";
+            lblvalor2.Click += label1_Click;
+            // 
+            // lblvalor1
+            // 
+            lblvalor1.AutoSize = true;
+            lblvalor1.Location = new Point(94, 60);
+            lblvalor1.Name = "lblvalor1";
+            lblvalor1.Size = new Size(42, 15);
+            lblvalor1.TabIndex = 3;
+            lblvalor1.Text = "Valor 1";
+            // 
+            // LblTotal
+            // 
+            LblTotal.AutoSize = true;
+            LblTotal.Location = new Point(94, 180);
+            LblTotal.Name = "LblTotal";
+            LblTotal.Size = new Size(32, 15);
+            LblTotal.TabIndex = 4;
+            LblTotal.Text = "Total";
+            // 
+            // LblResultado
+            // 
+            LblResultado.AutoSize = true;
+            LblResultado.Location = new Point(142, 180);
+            LblResultado.Name = "LblResultado";
+            LblResultado.Size = new Size(16, 15);
+            LblResultado.TabIndex = 5;
+            LblResultado.Text = "...";
+            // 
+            // btnsoma
+            // 
+            btnsoma.Font = new Font("Segoe UI", 9F);
+            btnsoma.Location = new Point(115, 240);
+            btnsoma.Name = "btnsoma";
+            btnsoma.Size = new Size(75, 23);
+            btnsoma.TabIndex = 6;
+            btnsoma.Text = "Soma";
+            btnsoma.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(678, 450);
+            Controls.Add(btnsoma);
+            Controls.Add(LblResultado);
+            Controls.Add(LblTotal);
+            Controls.Add(lblvalor1);
+            Controls.Add(lblvalor2);
+            Controls.Add(TbValor2);
+            Controls.Add(TbValor1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox TbValor1;
+        private TextBox TbValor2;
+        private Label lblvalor2;
+        private Label lblvalor1;
+        private Label LblTotal;
+        private Label LblResultado;
+        private Button btnsoma;
     }
 }
