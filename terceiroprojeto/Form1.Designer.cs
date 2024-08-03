011ae3994d34d15688af9487610f0827b25cd5b5
henriqbr14 <henriqschelotag@gmail.com>
2024-08-02 21:30:06 -0300

    Segundo passo

--- terceiroprojeto/Form1.Designer.cs
+++ terceiroprojeto/Form1.Designer.cs
@@ -35,18 +35,38 @@
             LblTotal = new Label();
             LblResultado = new Label();
             btnsoma = new Button();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            btnsoma1 = new Button();
+            lblResultado1 = new Label();
+            lbltotal1 = new Label();
+            lblvalor3 = new Label();
+            lblvalor4 = new Label();
+            txtvalor2 = new TextBox();
+            txtvalor = new TextBox();
+            groupBox3 = new GroupBox();
+            btn3 = new Button();
+            resu2 = new Label();
+            total3 = new Label();
+            lbvalor = new Label();
+            lbvalor2 = new Label();
+            txtbox2 = new TextBox();
+            txtbox1 = new TextBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
             SuspendLayout();
             // 
             // TbValor1
             // 
-            TbValor1.Location = new Point(142, 60);
+            TbValor1.Location = new Point(98, 43);
             TbValor1.Name = "TbValor1";
             TbValor1.Size = new Size(100, 23);
             TbValor1.TabIndex = 0;
             // 
             // TbValor2
             // 
-            TbValor2.Location = new Point(142, 118);
+            TbValor2.Location = new Point(98, 101);
             TbValor2.Name = "TbValor2";
             TbValor2.Size = new Size(100, 23);
             TbValor2.TabIndex = 1;
@@ -54,7 +74,7 @@
             // lblvalor2
             // 
             lblvalor2.AutoSize = true;
-            lblvalor2.Location = new Point(94, 121);
+            lblvalor2.Location = new Point(50, 104);
             lblvalor2.Name = "lblvalor2";
             lblvalor2.Size = new Size(42, 15);
             lblvalor2.TabIndex = 2;
@@ -64,7 +84,7 @@
             // lblvalor1
             // 
             lblvalor1.AutoSize = true;
-            lblvalor1.Location = new Point(94, 60);
+            lblvalor1.Location = new Point(50, 43);
             lblvalor1.Name = "lblvalor1";
             lblvalor1.Size = new Size(42, 15);
             lblvalor1.TabIndex = 3;
@@ -73,7 +93,7 @@
             // LblTotal
             // 
             LblTotal.AutoSize = true;
-            LblTotal.Location = new Point(94, 180);
+            LblTotal.Location = new Point(50, 163);
             LblTotal.Name = "LblTotal";
             LblTotal.Size = new Size(32, 15);
             LblTotal.TabIndex = 4;
@@ -82,7 +102,7 @@
             // LblResultado
             // 
             LblResultado.AutoSize = true;
-            LblResultado.Location = new Point(142, 180);
+            LblResultado.Location = new Point(98, 163);
             LblResultado.Name = "LblResultado";
             LblResultado.Size = new Size(16, 15);
             LblResultado.TabIndex = 5;
@@ -91,29 +111,201 @@
             // btnsoma
             // 
             btnsoma.Font = new Font("Segoe UI", 9F);
-            btnsoma.Location = new Point(115, 240);
+            btnsoma.Location = new Point(71, 223);
             btnsoma.Name = "btnsoma";
             btnsoma.Size = new Size(75, 23);
             btnsoma.TabIndex = 6;
             btnsoma.Text = "Soma";
             btnsoma.UseVisualStyleBackColor = true;
+            btnsoma.Click += btnsoma_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnsoma);
+            groupBox1.Controls.Add(LblResultado);
+            groupBox1.Controls.Add(LblTotal);
+            groupBox1.Controls.Add(lblvalor1);
+            groupBox1.Controls.Add(lblvalor2);
+            groupBox1.Controls.Add(TbValor2);
+            groupBox1.Controls.Add(TbValor1);
+            groupBox1.Location = new Point(44, 17);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(226, 267);
+            groupBox1.TabIndex = 7;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btnsoma1);
+            groupBox2.Controls.Add(lblResultado1);
+            groupBox2.Controls.Add(lbltotal1);
+            groupBox2.Controls.Add(lblvalor3);
+            groupBox2.Controls.Add(lblvalor4);
+            groupBox2.Controls.Add(txtvalor2);
+            groupBox2.Controls.Add(txtvalor);
+            groupBox2.Location = new Point(306, 17);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(226, 267);
+            groupBox2.TabIndex = 8;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // btnsoma1
+            // 
+            btnsoma1.Font = new Font("Segoe UI", 9F);
+            btnsoma1.Location = new Point(71, 223);
+            btnsoma1.Name = "btnsoma1";
+            btnsoma1.Size = new Size(75, 23);
+            btnsoma1.TabIndex = 6;
+            btnsoma1.Text = "Soma";
+            btnsoma1.UseVisualStyleBackColor = true;
+            btnsoma1.Click += btnsoma1_Click;
+            // 
+            // lblResultado1
+            // 
+            lblResultado1.AutoSize = true;
+            lblResultado1.Location = new Point(98, 163);
+            lblResultado1.Name = "lblResultado1";
+            lblResultado1.Size = new Size(16, 15);
+            lblResultado1.TabIndex = 5;
+            lblResultado1.Text = "...";
+            // 
+            // lbltotal1
+            // 
+            lbltotal1.AutoSize = true;
+            lbltotal1.Location = new Point(50, 163);
+            lbltotal1.Name = "lbltotal1";
+            lbltotal1.Size = new Size(32, 15);
+            lbltotal1.TabIndex = 4;
+            lbltotal1.Text = "Total";
+            // 
+            // lblvalor3
+            // 
+            lblvalor3.AutoSize = true;
+            lblvalor3.Location = new Point(50, 43);
+            lblvalor3.Name = "lblvalor3";
+            lblvalor3.Size = new Size(42, 15);
+            lblvalor3.TabIndex = 3;
+            lblvalor3.Text = "Valor 1";
+            // 
+            // lblvalor4
+            // 
+            lblvalor4.AutoSize = true;
+            lblvalor4.Location = new Point(50, 104);
+            lblvalor4.Name = "lblvalor4";
+            lblvalor4.Size = new Size(42, 15);
+            lblvalor4.TabIndex = 2;
+            lblvalor4.Text = "Valor 2";
+            // 
+            // txtvalor2
+            // 
+            txtvalor2.Location = new Point(98, 101);
+            txtvalor2.Name = "txtvalor2";
+            txtvalor2.Size = new Size(100, 23);
+            txtvalor2.TabIndex = 1;
+            // 
+            // txtvalor
+            // 
+            txtvalor.Location = new Point(98, 43);
+            txtvalor.Name = "txtvalor";
+            txtvalor.Size = new Size(100, 23);
+            txtvalor.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(btn3);
+            groupBox3.Controls.Add(resu2);
+            groupBox3.Controls.Add(total3);
+            groupBox3.Controls.Add(lbvalor);
+            groupBox3.Controls.Add(lbvalor2);
+            groupBox3.Controls.Add(txtbox2);
+            groupBox3.Controls.Add(txtbox1);
+            groupBox3.Location = new Point(570, 17);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(226, 267);
+            groupBox3.TabIndex = 9;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "groupBox3";
+            // 
+            // btn3
+            // 
+            btn3.Font = new Font("Segoe UI", 9F);
+            btn3.Location = new Point(71, 223);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(75, 23);
+            btn3.TabIndex = 6;
+            btn3.Text = "Soma";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // resu2
+            // 
+            resu2.AutoSize = true;
+            resu2.Location = new Point(98, 163);
+            resu2.Name = "resu2";
+            resu2.Size = new Size(16, 15);
+            resu2.TabIndex = 5;
+            resu2.Text = "...";
+            // 
+            // total3
+            // 
+            total3.AutoSize = true;
+            total3.Location = new Point(50, 163);
+            total3.Name = "total3";
+            total3.Size = new Size(32, 15);
+            total3.TabIndex = 4;
+            total3.Text = "Total";
+            // 
+            // lbvalor
+            // 
+            lbvalor.AutoSize = true;
+            lbvalor.Location = new Point(50, 43);
+            lbvalor.Name = "lbvalor";
+            lbvalor.Size = new Size(42, 15);
+            lbvalor.TabIndex = 3;
+            lbvalor.Text = "Valor 1";
+            // 
+            // lbvalor2
+            // 
+            lbvalor2.AutoSize = true;
+            lbvalor2.Location = new Point(50, 104);
+            lbvalor2.Name = "lbvalor2";
+            lbvalor2.Size = new Size(42, 15);
+            lbvalor2.TabIndex = 2;
+            lbvalor2.Text = "Valor 2";
+            // 
+            // txtbox2
+            // 
+            txtbox2.Location = new Point(98, 101);
+            txtbox2.Name = "txtbox2";
+            txtbox2.Size = new Size(100, 23);
+            txtbox2.TabIndex = 1;
+            // 
+            // txtbox1
+            // 
+            txtbox1.Location = new Point(98, 43);
+            txtbox1.Name = "txtbox1";
+            txtbox1.Size = new Size(100, 23);
+            txtbox1.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(678, 450);
-            Controls.Add(btnsoma);
-            Controls.Add(LblResultado);
-            Controls.Add(LblTotal);
-            Controls.Add(lblvalor1);
-            Controls.Add(lblvalor2);
-            Controls.Add(TbValor2);
-            Controls.Add(TbValor1);
+            ClientSize = new Size(1011, 620);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
             Name = "Form1";
             Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -125,5 +317,22 @@
         private Label LblTotal;
         private Label LblResultado;
         private Button btnsoma;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Button btnsoma1;
+        private Label lblResultado1;
+        private Label lbltotal1;
+        private Label lblvalor3;
+        private Label lblvalor4;
+        private TextBox txtvalor2;
+        private TextBox txtvalor;
+        private GroupBox groupBox3;
+        private Button btn3;
+        private Label resu2;
+        private Label total3;
+        private Label lbvalor;
+        private Label lbvalor2;
+        private TextBox txtbox2;
+        private TextBox txtbox1;
     }
 }
--- terceiroprojeto/Form1.cs
+++ terceiroprojeto/Form1.cs
@@ -11,5 +11,35 @@ namespace terceiroprojeto
         {
 
         }
+
+        private void btnsoma_Click(object sender, EventArgs e)
+        {
+            int var1, var2, var3, total;
+            var1 = int.Parse(TbValor1.Text);
+            var2 = int.Parse(TbValor2.Text);
+            total = var1 + var2;
+
+            LblResultado.Text = total.ToString();
+        }
+
+        private void btnsoma1_Click(object sender, EventArgs e)
+        {
+            int var1, var2, var3, total;
+            var1 = int.Parse(txtvalor.Text);
+            var2 = int.Parse(txtvalor2.Text);
+
+            total = var1 - var2;
+            lblResultado1.Text = total.ToString();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            int var1,var2,var3,total;
+            var1 = int.Parse(txtbox1.Text);
+            var2 = int.Parse(txtbox2.Text);
+            total = var1 * var2;
+            resu2.Text = total.ToString();
+
+        }
     }
 }
